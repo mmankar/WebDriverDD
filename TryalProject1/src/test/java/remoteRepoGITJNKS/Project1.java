518a0803762f7c1351edb06479db3ea12d497024
unknown <mmankar.personnel@gmail.com>
2020-06-27 22:41:44 +0530

    "Traial2"

---
+++ TryalProject1/src/test/java/remoteRepoGITJNKS/Project1.java
@@ -0,0 +1,39 @@
+package remoteRepoGITJNKS;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.BeforeSuite;
+import org.testng.annotations.Test;
+import org.testng.asserts.SoftAssert;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Project1 
+{
+	public static WebDriver driver;
+@BeforeSuite
+public void setUP()
+{
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+}
+@Test
+public void doLogin()
+{
+	driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+	driver.findElement(By.id("login1")).sendKeys("nilmangesh");
+	//SoftAssert softassert=new SoftAssert();
+	//softassert.assertAll();
+	
+}
+@AfterSuite
+public void tearDown() throws InterruptedException
+{
+	Thread.sleep(6000);
+	driver.close();
+	}
+}
