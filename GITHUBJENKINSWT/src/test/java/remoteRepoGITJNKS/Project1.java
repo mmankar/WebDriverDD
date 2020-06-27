347947ff5da0d96ba50f00510a70714e5e00a0a2
unknown <mmankar.personnel@gmail.com>
2020-06-27 21:23:13 +0530

    "NEWUPDATE"

--- GITHUBJENKINSWT/src/test/java/remoteRepoGITJNKS/Project1.java
+++ GITHUBJENKINSWT/src/test/java/remoteRepoGITJNKS/Project1.java
@@ -3,8 +3,8 @@ package remoteRepoGITJNKS;
 import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
-import org.testng.annotations.AfterMethod;
-import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.AfterSuite;
+import org.testng.annotations.BeforeSuite;
 import org.testng.annotations.Test;
 
 import io.github.bonigarcia.wdm.WebDriverManager;
@@ -12,7 +12,7 @@ import io.github.bonigarcia.wdm.WebDriverManager;
 public class Project1 
 {
 	public static WebDriver driver;
-@BeforeMethod
+@BeforeSuite
 public void setUP()
 {
 	WebDriverManager.chromedriver().setup();
@@ -22,9 +22,9 @@ public void setUP()
 public void doLogin()
 {
 	driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
-	driver.findElement(By.id("logi1")).sendKeys("nilmangesh");
+	driver.findElement(By.id("login1")).sendKeys("nilmangesh");
 }
-@AfterMethod
+@AfterSuite
 public void closeBrowser()
 {
 	driver.close();
