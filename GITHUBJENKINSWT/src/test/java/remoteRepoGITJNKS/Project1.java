8f7742832e82b3de8411f99eabbd3d19c5fe0841
unknown <mmankar.personnel@gmail.com>
2020-06-27 15:03:37 +0530

    "Changes done in Tea Qty1."

---
+++ GITHUBJENKINSWT/src/test/java/remoteRepoGITJNKS/Project1.java
@@ -0,0 +1,32 @@
+package remoteRepoGITJNKS;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.chrome.ChromeDriver;
+import org.testng.annotations.AfterMethod;
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+
+import io.github.bonigarcia.wdm.WebDriverManager;
+
+public class Project1 
+{
+	public static WebDriver driver;
+@BeforeMethod
+public void setUP()
+{
+	WebDriverManager.chromedriver().setup();
+	driver = new ChromeDriver();
+}
+@Test
+public void doLogin()
+{
+	driver.get("https://mail.rediff.com/cgi-bin/login.cgi");
+	driver.findElement(By.id("login1")).sendKeys("nilmangesh");
+}
+@AfterMethod
+public void closeBrowser()
+{
+	driver.close();
+	}
+}
